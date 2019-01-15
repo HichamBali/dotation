78c7497101ecc37ef916e7683a791a1a975d73fc
HichamBali <balihicham01@gmail.com>
2019-01-16 00:48:07 +0100

    bouton smart

--- models/extendingemploye.py
+++ models/extendingemploye.py
@@ -1,4 +1,13 @@
-from odoo import fields , models
+from odoo import fields , models , api
 
 class extendingemploye(models.Model):
     _inherit = "hr.employee"
+
+    decharge_count = fields.Integer(compute='_decharge_count', string='# of decharges')
+    decharge = fields.One2many('decharge', 'employe', 'dechargelemployer')
+
+    @api.one
+    @api.depends('decharge')
+    def _decharge_count(self):
+        decharges = self.env['decharge'].search([('employe', '=', self.ids)])
+        self.decharge_count = len(decharges)
